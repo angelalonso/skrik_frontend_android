b93fde3492b494d0d3045119bc5ba7ef61bace85
Ángel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2015-02-05 17:56:25 +0100

    Starting the develop of Chat

--- app/src/main/java/com/afonseca/skrik/ChatActivity.java
+++ app/src/main/java/com/afonseca/skrik/ChatActivity.java
@@ -1,10 +1,13 @@
 package com.afonseca.skrik;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by afonseca on 2/3/2015.
@@ -43,6 +46,7 @@ public class ChatActivity extends ActionBarActivity {
 
         if (serverSide.matches("OK")) { backend.updateNewslist(newsSQLHandler, userid); }
         //if (serverSide.matches("OK")) { Log.i("TESTING", userid); }
+        showMessages(other_userid);
 
     }
 
@@ -54,9 +58,45 @@ public class ChatActivity extends ActionBarActivity {
         serverSide = serverCheck(context);
 
         if (serverSide.matches("OK")) { Log.i("TESTING", userid); }
+
+        showMessages(other_userid);
     }
 
     /* Additional Actions' Methods */
+
+    private void showMessages(String user_other) {
+        Context mContext = getApplicationContext();
+
+        ArrayList<Data_ChatListItems> chatList = new ArrayList<>();
+        chatList.clear();
+
+        //String query = "SELECT * FROM PHONE_CONTACTS ";
+        String query = "SELECT CASE WHEN userid_from =30660416779715 THEN 'FROM' ELSE 'TO' END as to_or_from, id, message, timestamp, status from msging where userid_from ='" + user_other + "' or userid_to ='" + user_other + "' order by timestamp;";
+
+        Cursor c1 = newsSQLHandler.selectQuery(query);
+        if (c1 != null && c1.getCount() > 0) {
+            if (c1.moveToFirst()) {
+                do {
+                    Data_ChatListItems chatListItems = new Data_ChatListItems();
+
+                    chatListItems.setMsg(c1.getString(c1
+                            .getColumnIndex("message")));
+                    chatListItems.setToOrFromMe(c1.getString(c1
+                            .getColumnIndex("to_or_from")));
+                    chatListItems.setTimestamp(c1.getString(c1
+                            .getColumnIndex("timestamp")));
+
+                    chatList.add(chatListItems);
+
+                } while (c1.moveToNext());
+            }
+        }
+//TODO: Continue here
+        Control_NewsListAdapter contactListAdapter = new Control_NewsListAdapter(
+                NewsActivity.this, contactList);
+        NewsList_lv.setAdapter(contactListAdapter);
+
+    }
 
     /* Check Functions */
 
---
+++ app/src/main/java/com/afonseca/skrik/Data_ChatListItems.java
@@ -0,0 +1,37 @@
+package com.afonseca.skrik;
+
+/**
+ * Created by aaf on 2/5/15.
+ */
+public class Data_ChatListItems {
+
+    String message;
+    String to_or_from_me;
+    String timestamp;
+
+
+
+    public String getMsg() {
+        return message;
+    }
+
+    public void setMsg(String message) {
+        this.message = message;
+    }
+
+    public String getToOrFromMe() {
+        return to_or_from_me;
+    }
+
+    public void setToOrFromMe(String to_or_from_me) {
+        this.to_or_from_me = to_or_from_me;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+}
