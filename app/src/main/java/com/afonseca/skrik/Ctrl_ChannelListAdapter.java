67094720d0d530cfa94a0d2a3a1b07e5689249b7
afonseca <alonsofonseca.angel@gmail.com>
2015-03-14 04:39:29 +0100

    conversation show who sends

--- app/src/main/java/com/afonseca/skrik/Ctrl_ChannelListAdapter.java
+++ app/src/main/java/com/afonseca/skrik/Ctrl_ChannelListAdapter.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -56,6 +58,23 @@ public class Ctrl_ChannelListAdapter extends BaseAdapter {
         TextView Timestamp_tv = (TextView) convertView.findViewById(R.id.timestamp_tv);
         Timestamp_tv.setText(channelItems.getTimestamp());
 
+        String sender = channelItems.getToOrFromMe();
+        TextView Sender_tv = (TextView) convertView.findViewById(R.id.sender_tv);
+        if (sender.matches("FROM")) {
+            Sender_tv.setText("<");
+        } else {
+            Sender_tv.setText(">");
+            RelativeLayout.LayoutParams params = new
+                    RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,RelativeLayout.LayoutParams.WRAP_CONTENT);
+            params.addRule(RelativeLayout.LEFT_OF, R.id.sender_tv);
+            Message_tv.setLayoutParams(params);
+            RelativeLayout.LayoutParams params_b = new
+
+                    RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,RelativeLayout.LayoutParams.FILL_PARENT);
+            // TODO: Above, FILL_PARENT WORKS but we need an alternative that is not deprecated
+            params_b.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+            Sender_tv.setLayoutParams(params_b);
+        }
 
         return convertView;
     }
